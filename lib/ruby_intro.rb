ad955a351cdb0603e6fa8946cef7aae2cae06294
loantranuet <loanminie176@gmail.com>
2016-05-21 17:10:07 +0000

    First commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,92 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+# Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements.
+def sum (arr)
+    s=0
+    arr.each{|a| s+=a}
+    return s
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+# Define a method max_2_sum(array) which takes an array of integers as an argument and returns the sum of its two largest elements.
+def max_2_sum (arr)
+    
+    s=0
+    if (arr.length == 1)
+        return arr[0]
+    end
+    
+    if (arr.length == 0)
+        return 0
+    end
+    
+    if (!arr.empty?)
+        arr=arr.sort{ |x,y| y <=> x }
+    end
+    
+    s= arr[0] + arr[1]
+    
+    return s
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+# Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n,
+def sum_to_n? (arr, n)
+    
+    count=false
+    
+    if !(arr.empty? || arr.length == 1)
+        count=!! arr.uniq.combination(2).detect{|a,b| a + b == n}   
+    end
+    
+    return count
 end
 
-# Part 2
+# # Part 2
 
+# Define a method hello(name) that takes a string representing a name and returns the string "Hello, " concatenated with the name.
 def hello(name)
-  # YOUR CODE HERE
+    
+    str = "Hello, #{name}"
+    return str 
+    
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+# Define a method starts_with_consonant?(s) that takes a string and returns true if it starts with a consonant and false otherwise.
+def starts_with_consonant? (s)
+    start = false
+    if ((s =~ /\A(?=[^aeiou])(?=[a-z])/i) == 0)
+        start = true
+    end
+    return start
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+# Define a method binary_multiple_of_4?(s) that takes a string and returns true if the string represents a binary number that is a multiple of 4.
+def binary_multiple_of_4? (s)
+    isIt = false
+    if((s.to_i(2).is_a? Integer) && (s[0] == '0' || s[0] == '1'))
+        if(s.to_i(2) % 4 == 0)
+            isIt = true
+        end
+    end
+    return isIt
 end
 
-# Part 3
+# # Part 3
 
+# Define a class BookInStock which represents a book with an ISBN number, isbn, and price of the book as a floating-point number, price, as attributes.
 class BookInStock
-# YOUR CODE HERE
+    
+    def initialize (isbn, price)
+        raise ArgumentError, "Argument not valid!" unless
+        !(!(price > 0) or !(isbn != ""))
+        @isbn = isbn
+        @price = price
+    end
+    
+    attr_accessor :isbn
+    attr_accessor :price
+    
+    def price_as_string ()
+       return "$#{'%.2f' % @price}" 
+    end
+    
 end
